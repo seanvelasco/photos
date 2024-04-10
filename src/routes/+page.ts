fb9c9da054d0d95e756d1768ddf556d6fb9f267f
Sean Velasco <seanvelasco@ieee.org>
2024-04-11 07:41:45 +0800

    Temporary mock photos

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -7,6 +7,173 @@ export const load = () => {
                 image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
             },
             {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            }, {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            }, {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+
+            {
+                id: '1',
+                title: 'Penny',
+                image: 'https://avatars.githubusercontent.com/u/67859044?v=4'
+            },
+            {
                 id: '2',
                 title: 'Penny',
                 image: 'https://pbs.twimg.com/profile_images/1778197795406794752/A8dRJwOj_400x400.jpg'
